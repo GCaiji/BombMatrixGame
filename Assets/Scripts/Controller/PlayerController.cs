bdfd3987a33a469b1433f15d4948b56eb39a5210
GCaiji <1789336651@qq.com>
2025-04-21 12:08:22 +0800

    创建角色动画控制器

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -13,18 +13,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject destinationMarker;
     [SerializeField] private ParticleSystem clickEffect;
 
+    [Header("Animation")]
+    [SerializeField] private Animator animator; // 新增动画控制器
+    public float walkSpeed = 3.5f; // 将行走速度设为 public
+
     private Camera mainCamera;
 
     void Awake()
     {
         mainCamera = Camera.main;
         destinationMarker.SetActive(false);
+        agent.speed = walkSpeed; // 设置 NavMeshAgent 的速度
     }
 
     void Update()
     {
         HandleMovementInput();
         UpdateDestinationMarker();
+        UpdateAnimation(); // 更新动画状态
     }
 
     private void HandleMovementInput()
@@ -69,5 +75,14 @@ public class PlayerController : MonoBehaviour
 
         destinationMarker.transform.position = agent.destination;
         destinationMarker.SetActive(true);
+    }
+
+    private void UpdateAnimation()
+    {
+        if (animator != null)
+        {
+            // 根据 NavMeshAgent 的速度设置动画参数
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+        }
     }
 }
