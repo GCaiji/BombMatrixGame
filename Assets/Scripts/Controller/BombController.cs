58c72686a16811f6ed65fe140c30c93452d06ca4
GCaiji <1789336651@qq.com>
2025-04-24 20:24:15 +0800

    炸弹引燃动画粒子效果

---
+++ Assets/Scripts/Controller/BombController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BombController : MonoBehaviour {
+    public ParticleSystem spark; // 拖入SparkEffect
+    public ParticleSystem smoke; // 拖入SmokeEffect
+
+    // 动画事件调用：开始燃烧
+    public void PlayParticles() {
+        spark.Play();
+        smoke.Play();
+    }
+
+    // 动画事件调用：停止燃烧
+    public void StopParticles() {
+        spark.Stop();
+        smoke.Stop();
+    }
+}
